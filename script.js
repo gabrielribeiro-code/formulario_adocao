13926f208d48b41c7caa37015dc7040e5487a273
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-03-27 08:33:47 -0300

    feat: adicionei input de sim e não

--- script.js
+++ script.js
@@ -12,12 +12,23 @@ form.addEventListener('submit', function(e){
     let email = document.getElementById('email').value;
     let telefone = document.getElementById('telefone').value;
     let cpf =  document.getElementById('cpf').value;
+    let idade = document.getElementById('idade').value;
+    let cidade = document.getElementById('cidade').value;
+    let moradia = document.getElementById('moradia').value;
+    let quintal = document.getElementById('quintal').value;
+    let horasAnimal = document.getElementById('horasAnimal').value;
+    let pet = document.getElementById('pet').value;
 
 
     document.getElementById('erroNome').textContent ='';
     document.getElementById('erroEmail').textContent ='';
     document.getElementById('erroTelefone').textContent ='';
-    document.getElementById('erroCPF').textContent ='';
+    document.getElementById('erroIdade').textContent ='';
+    document.getElementById('erroCidade').textContent ='';
+    document.getElementById('erroMoradia').textContent ='';
+    document.getElementById('erroQuintal').textContent ='';
+    document.getElementById('erroPet').textContent ='';
+     document.getElementById('erroHorasAnimal').textContent ='';
 
     if (nome.length < 3) {
 
@@ -34,21 +45,48 @@ form.addEventListener('submit', function(e){
     }
 
 
-    if(telefone.length < 8) {
+    if(telefone.length > 9) {
 
-        document.getElementById('erroTelefone').textContent = 'O telefone deve ter pelo menos 8 caracteres.';
+        document.getElementById('erroTelefone').textContent = 'O telefone deve ter apenas 9 caracteres.';
 
         valido = false;
 
     }
 
-    if(cpf.length < 8) {
+    if(cpf.length > 14) {
 
         document.getElementById('erroCPF').textContent = 'O CPF deve ter pelo menos 14 caracteres.';
 
         valido = false;
 
     }
+
+     if(idade.length < 3) {
+
+        document.getElementById('erroIdade').textContent = 'ERRO! Não é permitido idade com mais de 3 caracteres.';
+
+        valido = false;
+
+    }
+
+    if(cidade.length <= 32) {
+
+        document.getElementById('erroCidade').textContent = 'ERRO! Não é um nome de cidade com mais de 32 caracteres.';
+
+        valido = false;
+
+    }
+
+    if(horasAnimal.length < 24) {
+
+        document.getElementById('erroHorasAnimal').textContent = 'Erro! Não é possível que o animal fica mais de 24 horas sozinho!';
+
+        valido = false;
+
+    }
+
+
+
 
 
     if(valido){
@@ -62,6 +100,7 @@ form.addEventListener('submit', function(e){
         Email: ${email} <br>
         Telefone: ${telefone} <br>
         CPF: ${cpf} <br>
+        
         
         `;
 
